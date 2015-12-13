141b17b1cca260e5dd423b8180d311bef5476b19
existmaster <existmaster@naver.com>
2015-12-13 13:21:34 +0900

    String Util 추가

---
+++ string-util/src/main/java/com/existmaster/study/util/string/StringUtil.java
@@ -0,0 +1,35 @@
+package com.existmaster.study.util.string;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * Created by existmaster on 2015. 12. 6..
+ */
+public class StringUtil {
+
+    public String convertIntToString(int value, int digit) {
+        String result = Integer.toString(value);
+        if (result.length() > digit) throw new IllegalArgumentException();
+
+        while (result.length() < digit) {
+            result = "0" + result;
+        }
+
+        return result;
+    }
+
+    public String replaceText(String source, String searchText, String replaceText) {
+        String result = "";
+        int searchTextIndex;
+        String source_back = source;
+        while(source_back.contains(searchText)){
+            searchTextIndex =  source_back.indexOf(searchText);
+            String source_front = source_back.substring(0, searchTextIndex);
+            source_back = source_back.substring(searchTextIndex + searchText.length());
+            result = result + source_front + replaceText;
+        }
+        result += source_back;
+        return result;
+    }
+}
---
+++ string-util/src/test/java/StringUtilTest.java
@@ -0,0 +1,50 @@
+import com.existmaster.study.util.string.StringUtil;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+/**
+ * Created by existmaster on 2015. 12. 6..
+ */
+public class StringUtilTest {
+
+    private StringUtil stringUtil;
+
+    @Before
+    public void setUp() throws Exception {
+        stringUtil = new StringUtil();
+
+    }
+
+    @Test
+    public void inputvalue(){
+        assertEquals("0012", stringUtil.convertIntToString(12, 4));
+        assertEquals("00000350", stringUtil.convertIntToString(350, 8));
+    }
+
+    @Test
+    public void InputWrongValue(){
+        wrongValueCheck(350, 2);
+    }
+
+    private void wrongValueCheck(int value, int digit) {
+        try{
+            stringUtil.convertIntToString(value, digit);
+            fail();
+        }catch (IllegalArgumentException e){
+            return;
+        }
+    }
+
+    @Test
+    public void ReplaceText(){
+        assertEquals("ab22efg", stringUtil.replaceText("abcdefg", "cd", "22"));
+        assertEquals("abefg", stringUtil.replaceText("abcdefg", "cd", ""));
+        assertEquals("cccd",stringUtil.replaceText("abcd", "ab", "cc"));
+        assertEquals("cdcdcd", stringUtil.replaceText("ababab", "ab", "cd"));
+        assertEquals("abc", stringUtil.replaceText("abc", "33", "dd"));
+    }
+
+}
